85878a862dd3671856c4af5accf22bee1dc478da
Moshe Avni <gatkees@gmail.com>
2020-07-22 02:08:37 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.h1`
+  color: blue;
+  text-decoration: underline;
+  cursor: pointer;
+  font-family: sans-serif;
+`
 
 function App() {
   return (
-    <h1>
+    <Header>
       Hello World!
-    </h1>
+    </Header>
   );
 }
 
